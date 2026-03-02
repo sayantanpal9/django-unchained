42a2c75ff305a0bf6e0edf22c4a4af8b3a6a1509
sayantanpal9 <sayantanpal2006.24@kgpian.iitkgp.ac.in>
2026-03-02 22:08:39 +0530

    hello bye

--- README.md
+++
@@ -1,2 +0,0 @@
-# django-unchained
-learning django backend
--- webdev/webdev/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def home(request):
-    # return HttpResponse('hello this is the home page')
-    return render(request, 'index.html')
