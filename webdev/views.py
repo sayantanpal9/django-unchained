cfba9aeec113cffcd677950868c33a988c9314cd
sayantanpal9 <sayantanpal2006.24@kgpian.iitkgp.ac.in>
2026-03-02 11:37:35 +0530

    hello there

--- webdev/views.py
+++ webdev/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('hello this is the home page')
+    # return HttpResponse('hello this is the home page')
+    return render(request, 'index.html')
